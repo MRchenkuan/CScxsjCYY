cc0c031079a325dfd9d1f022a748a922a1171c21
chenkuan <chenkuan@gigold.com>
2016-03-15 14:17:01 +0800

    板块

--- index.php
+++ index.php
@@ -34,18 +34,74 @@
     <div class="content autoPadding">
         <div class="entrances">
             <div class="entrance floatleft">
-                <div> 媒体报道 </div>
-                <span> MORE </span>
+                <div class="wrap">
+                    <a href="javascript:///">
+                        <span class="title"> 媒体报道 </span>
+                        <span class="more"> MORE </span>
+                    </a>
+                </div>
             </div>
-            <div class="entrance floatright">入口</div>
+            <div class="entrance floatright"><div class="wrap">
+                    <a href="javascript:///">
+                        <span class="title"> 园区动态 </span>
+                        <span class="more"> MORE </span>
+                    </a>
+                </div>
+            </div>
         </div>
         <?php getNewBox($news)?>
         <?php getNewBox($news,"floatright")?>
     </div>
 
     <div class="content autoPadding">
+        <div class="entrances">
+            <div class="entrance floatleft">
+                <div class="wrap">
+                    <a href="javascript:///">
+                        <span class="title"> 行业资讯 </span>
+                        <span class="more"> MORE </span>
+                    </a>
+                </div>
+            </div>
+            <div class="entrance floatright"><div class="wrap">
+                    <a href="javascript:///">
+                        <span class="title"> 人物专访 </span>
+                        <span class="more"> MORE </span>
+                    </a>
+                </div>
+            </div>
+        </div>
         <?php getNewBox($news)?>
         <?php getNewBox($news,"floatright")?>
     </div>
+
+    <!-- 走进园区部分 -->
+    <div class="content">
+        <div class="entrances fullwidth">
+            <div class="entrance fullwidth">
+                <div class="wrap">
+                    <a href="javascript:///">
+                        <span class="title"> 走进园区 </span>
+                        <span class="more"> MORE </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--  入驻企业部分  -->
+    <div class="content bottomshadow">
+        <div class="entrances fullwidth">
+            <div class="entrance fullwidth">
+                <div class="wrap">
+                    <a href="javascript:///">
+                        <span class="title"> 入驻企业 </span>
+                        <span class="more"> MORE </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- 底部 -->
+    <div class="footlink"></div>
     <?php getFoot()?>
